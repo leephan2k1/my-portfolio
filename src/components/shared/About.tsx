8cb2f7937b0bc1f678fe7d6dc7a22a54f1ce5fdd
leephan2k1 <pttl2k1@gmail.com>
2022-08-01 07:50:07 +0700

    style: 💄 medium screens responsive

--- src/components/shared/About.tsx
+++ src/components/shared/About.tsx
@@ -6,8 +6,8 @@ interface AboutProps {
 
 export default function About({ desc }: AboutProps) {
     return (
-        <article className="w-full min-h-[500px] h-[550px] max-h-[600px] overflow-hidden ">
-            <div className="flex flex-col justify-center py-2 px-6 items-center h-full lg:w-1/2 md:w-3/4 w-full mx-auto ">
+        <article className="w-full min-h-[500px] h-[550px] max-h-[600px] overflow-hidden">
+            <div className="flex flex-col justify-center md:justify-start py-2 px-6 items-center h-full lg:w-1/2 md:w-3/4 w-full mx-auto ">
                 <div className="flex">
                     <figure className="h-full min-w-fit absolute-center">
                         <img
