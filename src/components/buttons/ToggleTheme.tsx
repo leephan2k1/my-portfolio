8bd7457ac6eefc5205b1745d4821c77be34155ce
leephan2k1 <pttl2k1@gmail.com>
2022-08-02 13:14:10 +0700

    fix: 🐛 prop doesn't match server

--- src/components/buttons/ToggleTheme.tsx
+++ src/components/buttons/ToggleTheme.tsx
@@ -1,19 +1,22 @@
 import { useTheme } from 'next-themes';
 import { MoonIcon, SunIcon } from '@heroicons/react/outline';
+import ClientOnly from '../shared/ClientOnly';
 
 export default function ToggleTheme() {
     const { theme, setTheme } = useTheme();
 
     return (
-        <button
-            className="hover:scale-[120%] transition-all duration-200 rotate-6"
-            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-        >
-            {theme === 'light' ? (
-                <SunIcon className="w-8 h-8 hover:animate-spin" />
-            ) : (
-                <MoonIcon className="w-8 h-8 hover:animate-spin" />
-            )}
-        </button>
+        <ClientOnly>
+            <button
+                className="hover:scale-[120%] transition-all duration-200 rotate-6"
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+            >
+                {theme === 'light' ? (
+                    <SunIcon className="w-8 h-8 hover:animate-spin" />
+                ) : (
+                    <MoonIcon className="w-8 h-8 hover:animate-spin" />
+                )}
+            </button>
+        </ClientOnly>
     );
 }
